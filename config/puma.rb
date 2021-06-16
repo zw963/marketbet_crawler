e3107a7e3fdc2fa3c248f84bba624d3f9d416219
Billy.Zheng <vil963@gmail.com>
2021-06-16 19:08:29 +0800

    Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -2,4 +2,3 @@ threads_count = ENV.fetch("RACK_MAX_THREADS") { 1 }
 threads threads_count, threads_count
 port ENV.fetch("PORT") { 9393 }
 environment ENV.fetch("RACK_ENV") { "development" }
-log_requests true
--- config/puma_production.rb
+++ config/puma_production.rb
@@ -12,8 +12,10 @@ state_path "tmp/pids/#{file}.state"
 FileUtils.mkdir_p('log')
 stdout_redirect "log/production.log", "log/#{file}_err.log", true unless ENV['RACK_LOG_TO_STDOUT']
 
+# 当 workers 数量大于 1 时，默认会开启  preload_app! 设定，利用 CoW 节省内存。
+# 注意： preload_app! 无法和  phased restart feature 一起使用.
+# preload_app!
+
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-prune_bundler
-preload_app!
 
 plugin :tmp_restart
