3089121921e211f31eede2548574afd281eb32cd
Billy.Zheng <vil963@gmail.com>
2021-06-18 19:21:24 +0800

    Delete database_env

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,6 @@
-DB = Sequel.connect(ENV.fetch("#{ENV.fetch('RACK_ENV', "development").upcase}_DATABASE_URL"), timeout: 10000)
+# e.g DEVELOPMENT_DATABASE_URL
+database_url_name="#{ENV.fetch('RACK_ENV', "development").upcase}_DATABASE_URL"
+DB = Sequel.connect(ENV.delete(database_url_name) || ENV.delete('DATABASE_URL'), timeout: 10000)
 Sequel::Model.plugin :timestamps
 Sequel.extension :symbol_aref
 require 'sequel/extensions/blank'
