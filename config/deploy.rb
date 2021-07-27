a04ee48fdae094d3dd3abf1e3a27c76b6d50958d
Billy.Zheng <vil963@gmail.com>
2021-07-28 02:56:27 +0800

    fix db name

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,4 +3,4 @@ set :repo_url, 'git@github.com:zw963/marketbet_crawler'
 set :rvm_ruby_version, "ruby-2.7.2@#{fetch(:application)}"
 set :local_user, -> { Etc.getlogin }
 set :linked_dirs, %w{log tmp pids}
-set :linked_files, %w{.rvmrc}
+set :linked_files, %w{.rvmrc db/marketbet_crawler_production.db}
