0c133507b4f0e82e0ef687d8c953737f334cfc76
Billy.Zheng <vil963@gmail.com>
2021-07-31 23:27:49 +0800

    Update cap config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,4 @@
 set :application, 'marketbet_crawler'
 set :repo_url, 'git@github.com:zw963/marketbet_crawler'
-set :rvm_ruby_version, "ruby-2.7.2@#{fetch(:application)}"
-set :local_user, -> { Etc.getlogin }
 set :linked_dirs, %w{log tmp pids}
 set :linked_files, %w{.rvmrc db/marketbet_crawler_production.db}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
-role :app, ["deployer@152.32.169.103"]
+role :app, ["deployer2@152.32.169.103"]
 set :branch, 'test_cap1'
 set :deploy_to, "~/apps/#{fetch(:application)}_#{fetch(:stage)}"
