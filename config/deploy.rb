d61340d52b0794a9ebd62fe322deee5ad6d5a58c
Billy.Zheng <vil963@gmail.com>
2021-07-31 23:38:33 +0800

    Add rvm_ruby_version back

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 set :application, 'marketbet_crawler'
 set :repo_url, 'git@github.com:zw963/marketbet_crawler'
+set :rvm_ruby_version, '2.7.2'
 set :linked_dirs, %w{log tmp pids}
 set :linked_files, %w{.rvmrc db/marketbet_crawler_production.db}
