c49565f753848f2ec3162cd759e3ae83783fccad
Billy.Zheng <vil963@gmail.com>
2021-08-05 15:03:34 +0800

    Refactor

--- app/app.rb
+++ app/app.rb
@@ -6,36 +6,36 @@ class App < Roda
   plugin :path
 
   route do |r|
-    r.get 'stocks' do
-      sort_column, sort_direction = r.params.values_at('sort_column', 'sort_direction')
+    r.is 'stocks' do
+      r.get do
+        sort_column, sort_direction = r.params.values_at('sort_column', 'sort_direction')
 
-      if sort_column.present?
-        order = case sort_column
-                when /^(id|name|percent_of_institutions)$/
-                  :stocks[sort_column.to_sym]
-                when /^exchange_name$/
-                  :exchange[:name]
-                end
+        if sort_column.present?
+          order = case sort_column
+                  when /^(id|name|percent_of_institutions)$/
+                    :stocks[sort_column.to_sym]
+                  when /^exchange_name$/
+                    :exchange[:name]
+                  end
+        end
+
+        if sort_direction == 'desc'
+          order = order.desc
+        end
+
+        @stocks = Stock.association_join(:exchange).qualify.select_append(:exchange[:name].as(:exchange_name)).order(order)
+        view 'stocks/index', locals: {sort_column: sort_column, sort_direction: sort_direction}
       end
-
-      if sort_direction == 'desc'
-        order = order.desc
-      end
-
-      @stocks = Stock.association_join(:exchange).qualify.select_append(:exchange[:name].as(:exchange_name)).order(order)
-      view 'stocks/index', locals: {sort_column: sort_column, sort_direction: sort_direction}
     end
   end
 
-  # 第一次运行，生成的 url 是：<a href="/stocks/order_by=id&sort_direction=desc">
-  # 新的页面，会使用 stocks_path, 来生成所需的 link.
-  # 此时：
-
   path :stocks_link do |title, current_column, sort_column, sort_direction|
-    direction = current_column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-    query_string = URI.encode_www_form(sort_column: current_column, sort_direction: direction)
     href = "/stocks"
+
+    direction = (current_column == sort_column && sort_direction == 'desc') ? 'asc' : 'desc'
+    query_string = URI.encode_www_form(sort_column: current_column, sort_direction: direction)
     href = "#{href}?#{query_string}" if query_string.present?
+
     "<a href=\"#{href}\">#{title}</a>"
   end
 end
