0eaa60aff960c09872bfa610f5b6f2addf7f16fb
Billy.Zheng <vil963@gmail.com>
2021-08-07 23:38:33 +0800

    Pass @params to view, refactor to support query string.

--- app/app.rb
+++ app/app.rb
@@ -9,6 +9,7 @@ class App < Roda
   end
 
   route do |r|
+    @params = r.params
     r.is 'stocks' do
       r.get do
         @sort_column, @sort_direction = r.params.values_at('sort_column', 'sort_direction')
@@ -61,9 +62,12 @@ class App < Roda
     end
   end
 
-  path :link do |href, title, current_column, sort_column, sort_direction|
+  path :link do |href, title, current_column, params|
+    sort_column = params.delete('sort_column')
+    sort_direction = params.delete('sort_direction')
+
     direction = (current_column == sort_column && sort_direction == 'desc') ? 'asc' : 'desc'
-    query_string = URI.encode_www_form(sort_column: current_column, sort_direction: direction)
+    query_string = URI.encode_www_form(sort_column: current_column, sort_direction: direction, **params)
     href = "#{href}?#{query_string}" if query_string.present?
 
     "<a href=\"#{href}\">#{title}</a>"
