93796266634baafc93a8fa22f3147b4d02560020
Billy.Zheng <vil963@gmail.com>
2021-08-08 10:57:50 +0800

    Refactor link_path method

--- app/app.rb
+++ app/app.rb
@@ -62,10 +62,10 @@ class App < Roda
     end
   end
 
-  path :link do |href, title, current_column, params|
+  path :link do |href, title, current_column|
+    params = request.params.dup
     sort_column = params.delete('sort_column')
     sort_direction = params.delete('sort_direction')
-
     direction = (current_column == sort_column && sort_direction == 'desc') ? 'asc' : 'desc'
     query_string = URI.encode_www_form(sort_column: current_column, sort_direction: direction, **params)
     href = "#{href}?#{query_string}" if query_string.present?
