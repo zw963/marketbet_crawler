1188e10a77e946c7a87d973ce53884a80af9b22e
Billy.Zheng <vil963@gmail.com>
2021-08-09 21:11:07 +0800

    Set App.opts[:root] when start.

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,3 @@ require 'bundler'
 # 在生产环境, 添加export BUNDLE_WITHOUT=development:test, 来跳过所有不需要的 gem
 Bundler.require(:default, ENV.fetch('RACK_ENV', "development"))
 require_relative 'application'
-require_relative 'hot_reloader'
