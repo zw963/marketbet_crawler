bf0abb233b4ba58693199582da4d9b823c739e57
Billy.Zheng <vil963@gmail.com>
2021-08-12 16:13:29 +0800

    Refactor with require_tree

--- assets/js/app.rb
+++ assets/js/app.rb
@@ -1,28 +1,6 @@
 require 'opal'
 require 'snabberb'
 
-puts 'hello'
-
-class TextBox < Snabberb::Component
-  needs :text
-  needs :selected, default: false, store: true
-
-  def render
-    onclick = lambda do
-      store(:selected, !@selected)
-    end
-
-    style = {
-      cursor: 'pointer',
-      border: 'solid 1px rgba(0,0,0,0.2)',
-    }
-
-    style['background-color'] = 'lightblue' if @selected
-
-    h(:div, { style: style, on: { click: onclick } }, [
-      h(:div, @text)
-    ])
-  end
-end
+require_tree './components'
 
 TextBox.attach('app', text: 'hello world')
---
+++ assets/js/components/text_box.rb
@@ -0,0 +1,21 @@
+class TextBox < Snabberb::Component
+  needs :text
+  needs :selected, default: false, store: true
+
+  def render
+    onclick = lambda do
+      store(:selected, !@selected)
+    end
+
+    style = {
+      cursor: 'pointer',
+      border: 'solid 1px rgba(0,0,0,0.2)',
+    }
+
+    style['background-color'] = 'lightblue' if @selected
+
+    h(:div, { style: style, on: { click: onclick } }, [
+        h(:div, @text)
+      ])
+  end
+end
