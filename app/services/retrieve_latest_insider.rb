26f8deebb3928657bb67d674245f84527734b015
Billy.Zheng <vil963@gmail.com>
2021-08-13 18:38:41 +0800

    Add latest insider service

---
+++ app/services/retrieve_latest_insider.rb
@@ -0,0 +1,60 @@
+class RetrieveLatestInsider
+  include Interactor
+
+  def call
+    today = Date.today
+    now = today.to_datetime
+    days = context.days.to_i
+    order = context.order || :stock_id
+
+    insiders = Insider.where(
+      Sequel.or(
+        date: today-days..today,
+        created_at: now...(today + 1).to_datetime
+      )
+    ).order(order).all
+
+    mapping = {
+      'major shareholder' => "大股东",
+      "insider" => "内部人士",
+      "ceo" => "CEO",
+      "director" => "董事",
+      "cto" => "CTO",
+      "cfo" => "CFO",
+      "coo" => "首席运营官",
+      "general counsel" => "顾问",
+      "svp" => "高级副总裁",
+      "vp" => "副总裁",
+      "evp" => "执行副总裁",
+      "cao" => "首席行政官",
+      "vice chairman" => "副董事長",
+      "chairman" => "董事長",
+      "cmo" => "首席市场官",
+      'president' => '总裁'
+    }
+
+    if insiders.empty?
+      context.fail!(message: "没有最新的结果！")
+    else
+      context.insiders = insiders.map do |ins|
+        title = ins.title
+        stock = ins.stock
+
+        unless mapping[title.downcase].to_s.downcase == title.downcase
+          title = "#{title}(#{mapping[title.downcase]})"
+        end
+
+        {
+          'ID' => ins.id,
+          '股票' => "#{stock.exchange.name}/#{stock.name}",
+          '日期' => ins.date.to_s,
+          '名称' => ins.name,
+          '职位' => title,
+          '股票数量' => ins.number_of_shares,
+          '平均价格' => ins.average_price.to_f,
+          '交易价格' => ins.share_total_price.to_f,
+        }
+      end
+    end
+  end
+end
