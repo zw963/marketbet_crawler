1e73fe9b49d0f56bc4bf9e3a7ae17e915f430343
Billy.Zheng <vil963@gmail.com>
2021-08-24 18:52:45 +0800

    Fix bug.

--- app/graphql/types/query.rb
+++ app/graphql/types/query.rb
@@ -1,7 +1,7 @@
 class Types::Query < GraphQL::Schema::Object
   description('The query root of this schema')
 
-  field(:stock, Objects::StockType, null: true) do
+  field(:stock, Types::Objects::StockType, null: true) do
     description('Find a stock')
   end
 
