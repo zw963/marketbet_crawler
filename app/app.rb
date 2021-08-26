5fa1370ea9af03a0d302ec6a84950466d2d6321a
Billy.Zheng <vil963@gmail.com>
2021-08-26 21:29:09 +0800

    Refine code

--- app/app.rb
+++ app/app.rb
@@ -79,27 +79,36 @@ class App < Roda
         sort_column = sort_column.presence || :date
         sort_direction = sort_direction.presence || :desc
 
+        @log1 = Log.last(type: 'institution_parser')
         result = RetrieveInstitutions.call(sort_column: sort_column, sort_direction: sort_direction, stock_id: id)
+
+        cond1, cond2 = nil, nil
 
         if result.success?
           @institutions = result.institutions
         else
           @error_message = result.message
-          @error_message = "#{@error_message} 最后一次爬虫时间为: #{@log.finished_at}" if @log.present?
-          r.halt
+          @error_message = "#{@error_message}，机构最后一次爬虫时间为: #{@log1.finished_at}" if @log1.present?
+          cond1 = true
         end
 
+        @log2 = Log.last(type: 'insider_parser')
         result = RetrieveInsider.call(sort_column: sort_column, sort_direction: sort_direction, stock_id: id)
 
         if result.success?
           @insiders = result.insiders
         else
-          @error_message = result.message
-          @error_message = "#{@error_message} 最后一次爬虫时间为: #{@log.finished_at}" if @log.present?
-          r.halt
+          # @error_message = result.message
+          @error_message = "#{@error_message} 内幕交易最后一次爬虫时间为: #{@log2.finished_at}" if @log2.present?
+          cond2 = true
         end
 
-        view 'stocks/show'
+        p cond1, cond2
+        if cond1 && cond2
+          r.halt
+        else
+          view 'stocks/show'
+        end
       end
 
       r.is 'firms', Integer do |id|
