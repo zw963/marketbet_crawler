17a8efb3cba0925ce84e485803a71cabcc5b3340
Billy.Zheng <vil963@gmail.com>
2021-09-06 15:08:10 +0800

    Refactor use dropdown

--- assets/js/app.rb
+++ assets/js/app.rb
@@ -27,11 +27,19 @@ end
 $document.ready do
   get_json('/stocks.json').then do |json|
     change_firm_display_name_callback = proc do |_, trigger|
-      firm_id = `trigger.dataset.firmId`
-      firm_name = `trigger.dataset.firmName`
       form = $document.at_css('#modal1 form')
-      form.action = "/firms/#{firm_id}"
-      form.at_css('input').value = firm_name
+      form.action = "/firms/#{`trigger.dataset.firmId`}"
+      form.at_css('input').value = `trigger.dataset.firmName`
+      form.at_css('h4').text = '修改机构信息'
+    end
+
+    open_dropdown_callback = proc do |dropdown_trigger|
+      a_eles = $document.css('#dropdown1 li a')
+      a_eles[0]['href'] = `dropdown_trigger.href`
+      a_eles[0].text = '查看机构信息'
+      a_eles[1]['data-firm-id'] = `dropdown_trigger.dataset.firmId`
+      a_eles[1]['data-firm-name'] = `dropdown_trigger.innerText`
+      a_eles[1].text = '修改名称备注'
     end
 
     %x{
@@ -40,6 +48,9 @@ var instances = M.Autocomplete.init(elems, {"data": #{json.to_n}});
 
 var elems = document.querySelectorAll('.modal');
 var instances = M.Modal.init(elems, {"onOpenStart": #{change_firm_display_name_callback.to_n}});
+
+ var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {"onOpenStart": #{open_dropdown_callback.to_n}});
     }
   end
 end
