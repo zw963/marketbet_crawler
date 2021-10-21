ce96af1acf0a1603f14757f27d94e3f65f914380
Billy.Zheng <vil963@gmail.com>
2021-10-22 00:18:16 +0800

    Fix test.

--- test/services/latest_institutions_test.rb
+++ test/services/latest_institutions_test.rb
@@ -7,10 +7,10 @@ describe "retrieve latest institutions" do
 
     exchange = create(:exchange, name: 'exchange')
     stock = create(:stock, name: 'stock', exchange: exchange, id: 1)
-    firm1 = create(:firm)
-    firm2 = create(:firm, display_name: '黑石_2')
-    firm3 = create(:firm, display_name: '黑石_3')
-    firm4 = create(:firm, display_name: '黑石_4')
+    firm1 = create(:firm, id: 1)
+    firm2 = create(:firm, display_name: '黑石_2', id: 2)
+    firm3 = create(:firm, display_name: '黑石_3', id: 3)
+    firm4 = create(:firm, display_name: '黑石_4', id: 4)
     firm5 = create(:firm, display_name: '黑石_5', id: 5)
     create(:institution, date: '2021-08-02', created_at: '2021-08-04', name: 1, firm: firm1, stock: stock)
     create(:institution, date: '2021-08-03', created_at: '2021-08-04', name: 2, firm: firm2, stock: stock)
@@ -21,13 +21,13 @@ describe "retrieve latest institutions" do
     result = RetrieveInstitutions.call(days: 3, sort_column: 'stock_name')
     assert_equal true, result.success?
     institutions = result.institutions
-    assert_equal ["黑石_2", "黑石_3", "黑石_4", "黑石_5"], (institutions.map {|x| x['机构名称'] })
+    assert_equal ["黑石_5", "黑石_4", "黑石_3", "黑石_2"], (institutions.map {|x| x['机构名称'] })
     assert_equal({
       "股票"=>"exchange/stock",
       "stock_id" => 1,
-      "日期"=>"2021-08-02",
-      "机构名称"=>"黑石_5",
-      "firm_id" => 5,
+      "日期"=>"2021-08-03",
+      "机构名称"=>"黑石_2",
+      "firm_id" => 2,
       "机构持有数量"=>69830,
       "市场价值"=>"94.0万($0.94M)",
       "占股票百分比"=>"2.4%",
@@ -35,7 +35,7 @@ describe "retrieve latest institutions" do
       "机构季度变动百分比"=>"N/A",
       "机构季度变动数量"=>"N/A",
       "机构平均成本"=>0.0,
-      "创建时间"=>"08-06 00:00"
+      "创建时间"=>"08-04 00:00"
     },
       institutions.last.except("ID"))
   end
