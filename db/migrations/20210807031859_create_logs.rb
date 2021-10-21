23078ef239aa1f9b94474b79e86cd805139f9f8b
Billy.Zheng <vil963@gmail.com>
2021-10-22 00:04:55 +0800

    Fix db migration bug.

--- db/migrations/20210807031859_create_logs.rb
+++ db/migrations/20210807031859_create_logs.rb
@@ -3,7 +3,7 @@ Sequel.migration do
     create_table(:logs) do
       primary_key :id
       String :type, null: false
-      Datetime :finished_at
+      DateTime :finished_at
       DateTime :created_at, null: false
     end
   end
