c5e4ac0b17a2d2ab2b0e689693736fabbeeea703
Billy.Zheng <vil963@gmail.com>
2021-11-05 13:29:57 +0800

    Fix config/load_env.rb broken when value is not a string.

--- config/load_env.rb
+++ config/load_env.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 env_files = ['Procfile.options', 'Procfile.local']
 envs = env_files.filter_map {|file| YAML.load_file(file)['env'] if File.file?(file) }
-envs = {}.merge!(*envs)
+envs = {}.merge!(*envs).transform_values!(&:to_s)
 envs.reject! {|k, _v| ENV.key? k }
 ENV.merge!(envs)
