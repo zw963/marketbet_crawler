b346c6f0aa0867dfc6faf32b132f2f9d63d2d356
Billy.Zheng <vil963@gmail.com>
2021-11-06 20:29:28 +0800

    Add falcon.rb config file.

---
+++ falcon.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S falcon host
+# frozen_string_literal: true
+
+load :rack, :lets_encrypt_tls, :supervisor
+
+hostname = File.basename(__dir__)
+rack hostname, :lets_encrypt_tls do
+	cache true
+end
+
+supervisor
