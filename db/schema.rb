4306f67dfb182ee3580ad24b757891fa89a646a5
Billy.Zheng <vil963@gmail.com>
2021-11-09 18:34:34 +0800

    Add Insider model.

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,21 @@
 Sequel.migration do
   change do
     create_table(:exchanges, :ignore_index_errors=>true) do
-      primary_key :id
-      String :name, :text=>true, :null=>false
+      primary_key :id, :type=>:Bignum
+      String :name, :size=>255, :null=>false
       
-      index [:name], :name=>:exchanges_name_key, :unique=>true
+      index [:name], :name=>:sqlite_autoindex_exchanges_1, :unique=>true
     end
     
     create_table(:firms) do
+      primary_key :id, :type=>:Bignum
+      String :name, :size=>255, :null=>false
+      String :display_name, :size=>255
+    end
+    
+    create_table(:insiders) do
       primary_key :id
-      String :name, :text=>true, :null=>false
-      String :display_name, :text=>true
+      String :name, :text=>true
     end
     
     create_table(:investing_latest_news, :ignore_index_errors=>true) do
@@ -28,69 +33,69 @@ Sequel.migration do
     end
     
     create_table(:logs) do
-      primary_key :id
-      String :type, :text=>true, :null=>false
+      primary_key :id, :type=>:Bignum
+      String :type, :size=>255, :null=>false
       DateTime :finished_at
       DateTime :created_at, :null=>false
     end
     
     create_table(:schema_migrations) do
-      String :filename, :text=>true, :null=>false
+      String :filename, :size=>255, :null=>false
       
       primary_key [:filename]
     end
     
     create_table(:new_stocks) do
-      primary_key :id
-      String :name, :text=>true, :null=>false
-      foreign_key :exchange_id, :exchanges, :key=>[:id]
+      primary_key :id, :type=>:Bignum
+      String :name, :size=>255, :null=>false
+      foreign_key :exchange_id, :exchanges, :type=>:Bignum, :key=>[:id]
       BigDecimal :percent_of_institutions
     end
     
     create_table(:stocks, :ignore_index_errors=>true) do
-      primary_key :id
-      String :name, :text=>true, :null=>false
-      foreign_key :exchange_id, :exchanges, :key=>[:id]
+      primary_key :id, :type=>:Bignum
+      String :name, :size=>255, :null=>false
+      foreign_key :exchange_id, :exchanges, :type=>:Bignum, :key=>[:id]
       BigDecimal :percent_of_institutions
-      String :ipo_price, :text=>true
+      String :ipo_price, :size=>255
       BigDecimal :ipo_amount
-      String :ipo_placement, :text=>true
+      String :ipo_placement, :size=>255
       Date :ipo_date
       BigDecimal :ipo_average_price
       BigDecimal :ipo_placement_number
       Date :next_earnings_date
       
-      index [:name], :name=>:stocks_name_key, :unique=>true
+      index [:name], :name=>:sqlite_autoindex_stocks_1, :unique=>true
     end
     
     create_table(:insider_histories) do
-      primary_key :id
+      primary_key :id, :type=>:Bignum
       Date :date, :null=>false
-      String :name, :text=>true, :null=>false
-      String :title, :text=>true, :null=>false
-      Integer :number_of_holding
-      Integer :number_of_shares, :null=>false
+      String :name, :size=>255, :null=>false
+      String :title, :size=>255, :null=>false
+      Bignum :number_of_holding
+      Bignum :number_of_shares, :null=>false
       BigDecimal :average_price, :null=>false
       BigDecimal :share_total_price, :null=>false
       DateTime :created_at, :null=>false
-      foreign_key :stock_id, :stocks, :key=>[:id]
+      foreign_key :stock_id, :stocks, :type=>:Bignum, :key=>[:id]
     end
     
     create_table(:institutions) do
-      primary_key :id
-      String :name, :text=>true, :null=>false
-      Integer :number_of_holding, :null=>false
-      Integer :market_value
+      primary_key :id, :type=>:Bignum
+      String :name, :size=>255, :null=>false
+      Bignum :number_of_holding, :null=>false
+      Bignum :market_value
       BigDecimal :percent_of_shares_for_stock
       BigDecimal :percent_of_shares_for_institution
       BigDecimal :quarterly_changes_percent
-      Integer :quarterly_changes
-      String :market_value_dollar_string, :text=>true
+      Bignum :quarterly_changes
+      String :market_value_dollar_string, :size=>255
       BigDecimal :holding_cost
       Date :date
       DateTime :created_at
-      foreign_key :stock_id, :stocks, :key=>[:id]
-      foreign_key :firm_id, :firms, :key=>[:id]
+      foreign_key :stock_id, :stocks, :type=>:Bignum, :key=>[:id]
+      foreign_key :firm_id, :firms, :type=>:Bignum, :key=>[:id]
     end
   end
 end
