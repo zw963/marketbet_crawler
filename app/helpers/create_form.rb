3391cd55b1cd110d79cbee063a8c095508aad10e
Billy.Zheng <vil963@gmail.com>
2021-11-20 17:13:39 +0800

    refactor create_form.

--- app/helpers/create_form.rb
+++ app/helpers/create_form.rb
@@ -9,16 +9,26 @@ class App < Roda
     grid_offset: nil,
     onclick: "this.closest('form').submit(); return false;",
     autocomplete: "on",
-    size: 2
+    size: 2,
+    use_input: true
   )
-<<-HEREDOC
+
+    str1 = <<-HEREDOC
 <form action="#{action}" autocomplete="#{autocomplete}" method="#{method}">
   <div class="input-field col s#{size} #{grid_offset}">
-    <input type="text" name="#{param}" id="autocomplete-input" class="#{class_name}" value="#{request.params[param]}" aria-label="Search">
+HEREDOC
+
+    str2 = <<-HEREDOC
     <i class="material-icons prefix submit-icon" onclick="#{onclick}">#{icon}</i>
     <label for="autocomplete-input">#{placeholder}</label>
   </div>
 </form>
 HEREDOC
+
+    input = <<-HEREDOC if use_input
+    <input type="text" name="#{param}" id="autocomplete-input" class="#{class_name}" value="#{request.params[param]}" aria-label="Search">
+HEREDOC
+
+    str1 + input.to_s + str2
   end
 end
--- spec/app_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe App do
-  it "corrects" do
-    1.must_equal 1
-  end
-end
