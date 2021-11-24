7dc06c2cc76840084ba824a38ab61d7085b32b5f
Billy.Zheng <vil963@gmail.com>
2021-11-24 22:07:55 +0800

    Fix test.

--- test/routes/firms/update_test.rb
+++ test/routes/firms/update_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 describe 'routes/institutions/update' do
   it 'should update institution' do
     institution = create(:institution, id: 1, name: 'firm1')
-    assert_nil institution.display_name
+    assert_equal 'firm1', institution.display_name
     post '/institutions/1', {display_name: "new_institution"}
     assert last_response.redirect?
-    assert_equal 'new_institution', institution.reload.display_name
+    assert_equal "firm1 (new_institution)", institution.reload.display_name
   end
 end
--- test/services/latest_institution_histroies_test.rb
+++ test/services/latest_institution_histroies_test.rb
@@ -8,10 +8,10 @@ describe "retrieve latest institution histories" do
     exchange = create(:exchange, name: 'nyse')
     stock = create(:stock, name: 'nyse/ge', exchange: exchange, id: 1)
     institution1 = create(:institution, id: 1)
-    institution2 = create(:institution, display_name: '黑石_2', id: 2)
-    institution3 = create(:institution, display_name: '黑石_3', id: 3)
-    institution4 = create(:institution, display_name: '黑石_4', id: 4)
-    institution5 = create(:institution, display_name: '黑石_5', id: 5)
+    institution2 = create(:institution, display_name: '黑石_2', id: 2, name: 'Name2')
+    institution3 = create(:institution, id: 3, name: 'Name3')
+    institution4 = create(:institution, display_name: '黑石_4', id: 4, name: 'Name4')
+    institution5 = create(:institution, id: 5, name: 'Name5')
     create(:institution_history, date: '2021-08-02', created_at: '2021-08-04', institution: institution1, stock: stock)
     create(:institution_history, date: '2021-08-03', created_at: '2021-08-04', institution: institution2, stock: stock)
     create(:institution_history, date: '2021-08-04', created_at: '2021-08-04', institution: institution3, stock: stock)
@@ -21,12 +21,14 @@ describe "retrieve latest institution histories" do
     result = RetrieveInstitutionHistory.call(days: 3, sort_column: 'institution_id', sort_direction: 'desc')
     assert_equal true, result.success?
     institution_histories = result.institution_histories
-    assert_equal ["黑石_5", "黑石_4", "黑石_3", "黑石_2"], (institution_histories.map {|x| x['机构名称'] })
+    assert_equal [
+      "Name5", "Name4 (黑石_4)", "Name3", "Name2 (黑石_2)"
+    ], (institution_histories.map {|x| x['机构名称'] })
     assert_equal({
       "股票"=>"nyse/ge",
       "stock_id" => 1,
       "日期"=>"2021-08-03",
-      "机构名称"=>"黑石_2",
+      "机构名称"=>"Name2 (黑石_2)",
       "institution_id" => 2,
       "机构持有数量"=>182902,
       "市场价值"=>"94.0万($0.94M)",
