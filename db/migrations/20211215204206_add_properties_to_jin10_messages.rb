120b1d2947cd1b4af39ce706d3a2deeba0dee52a
Billy.Zheng <vil963@gmail.com>
2021-12-29 19:46:56 +0800

    add pg db detect.

--- db/migrations/20211215204206_add_properties_to_jin10_messages.rb
+++ db/migrations/20211215204206_add_properties_to_jin10_messages.rb
@@ -1,9 +1,11 @@
-DB.extension :pg_json
-
 Sequel.migration do
   change do
-    alter_table(:jin10_messages) do
-      add_column :properties, :jsonb, default: '{}'
+    if DB.adapter_scheme == :postgres
+      DB.extension :pg_json
+
+      alter_table(:jin10_messages) do
+        add_column :properties, :jsonb, default: '{}'
+      end
     end
   end
 end
