57c9c425a69abb342820b1384235dc733ce5ccee
Billy.Zheng <vil963@gmail.com>
2023-02-11 03:39:34 +0800

    Fix bug.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 function set_backup_policy () {
-    set -eu
+    set -eux
 
     backup_path=$1
-    max_backup_count=$2
+    max_backup_count=$((${2}-0))
 
-    oldest_backup_dir="${backup_path}".$(($max_backup_count-1))
+    oldest_backup_dir="${backup_path}".$max_backup_count
+    [[ $max_backup_count -lt 10 ]] && oldest_backup_dir="${backup_path}".0$max_backup_count
     newest_backup_dir="${backup_path}"
 
     # 如果无编号的 backup 目录不存在, 则直接创建它, 并返回.(即: 无需后续的轮回策略.)
@@ -22,7 +23,7 @@ function set_backup_policy () {
     # 如果已经达到最大备份, 删除最老的备份.
     if [ -d "$oldest_backup_dir" ]; then
         echo "Deleting oldest backup dir $oldest_backup_dir, it may need several minutes ..."
-        quick_rmdir "$oldest_backup_dir"
+        rm -rf "$oldest_backup_dir"
         echo "Done."
     fi
 
@@ -50,17 +51,15 @@ echo "-----> Fetching new git commits"
 (cd $home/scm && git fetch git@github.com:zw963/marketbet_crawler master:master --force) &&
     echo "-----> Using git branch master" &&
     cd $home &&
-    set_backup_policy $current 07 &&
+    set_backup_policy $current 7 &&
     git clone scm $current --recursive --branch master &&
     cd $current && git rev-parse HEAD > .git_revision &&
-    git --no-pager log --format="%aN (%h):%n> %s" -n 1
-
-cd $home/current &&
-    ln -sf ../shards/.rvmrc . &&
-    ln -sf ../shards/Procfile . &&
-    ln -sf ../shards/Procfile.local . &&
-    ln -sf ../shards/log . &&
-    ln -sf ../shards/tmp . &&
-    ln -sf ../shards/pids . &&
-    ln -sf ../shards/public/assets public/assets &&
-    ln -sf ../shards/db/files db/files
+    git --no-pager log --format="%aN (%h):%n> %s" -n 1 &&
+    ln -sf ../../shared/.rvmrc . &&
+    ln -sf ../../shared/Procfile . &&
+    ln -sf ../../shared/Procfile.local . &&
+    ln -sf ../../shared/log . &&
+    ln -sf ../../shared/tmp . &&
+    ln -sf ../../shared/pids . &&
+    ln -sf ../../shared/public/assets public/assets &&
+    ln -sf ../../shared/db/files db/files
