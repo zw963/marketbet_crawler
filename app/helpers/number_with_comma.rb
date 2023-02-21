303f911398669569306eb9f569d20d0978b1be9b
Billy.Zheng <vil963@gmail.com>
2023-02-21 23:28:43 +0800

    Fix bug.

--- app/helpers/number_with_comma.rb
+++ app/helpers/number_with_comma.rb
@@ -2,6 +2,8 @@ module Helpers
   module_function
 
   def number_with_comma(number, space: 4)
+    return '' if number.nil?
+
     int, dec = number.to_s.split('.')
     int = int.gsub(/\B(?=(#{"." * space})*\b)/, ',')
     int = "#{int}.#{dec}" unless dec.nil?
