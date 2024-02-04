56fdf7b90d537c05a6896bebfbab902a3d1e9395
Billy.Zheng <vil963@gmail.com>
2024-02-05 01:06:28 +0800

    Fix wrong comma foramt.

--- app/helpers/number_with_comma.rb
+++ app/helpers/number_with_comma.rb
@@ -5,7 +5,7 @@ module Helpers
     return '' if number.nil?
 
     int, dec = number.to_s.split('.')
-    int = int.gsub(/\B(?=(#{"." * space})*\b)/, ',')
+    int = int.gsub(/\B(?=(#{"." * space})*\b)/, ',') if int.size > 4
     int = "#{int}.#{dec}" unless dec.nil?
     int
   end
